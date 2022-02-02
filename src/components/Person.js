ed8924bc0caef9705228b682d20f8fd555813013
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-02-02 14:51:40 +0300

    got one person

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 
 const Person = () => {
-  return <div>Person</div>;
+  const api = 'https://rickandmortyapi.com/api/character/5';
+
+  const [allData, setAllData] = useState([]);
+  const { name } = allData;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      setAllData(data);
+    })();
+  }, [api]);
+
+  return (
+    <div>
+      <ul>{name}</ul>
+    </div>
+  );
 };
 
 export default Person;
