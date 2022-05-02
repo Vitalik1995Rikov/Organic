23da13e666e100d9c59692c69d6c0f439021293d
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-02 17:59:52 +0300

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 let Header = function () {
   return (
-    <div>
-      <button>organick</button>
+    <div className="py-10">
+      <button>Organick</button>
+      <button>Home</button>
+      <button>About Us</button>
+      <button>Shop</button>
+      <button>Pages</button>
+      <button>Contact Us</button>
+      <button>Basket</button>
     </div>
   );
 };
