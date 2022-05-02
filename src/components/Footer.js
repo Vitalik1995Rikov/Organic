8ef0c35cb5e97385baab127114beafe3c6ffc832
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-02 21:10:23 +0300

    changed Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,23 @@
 let Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex">
+      <div>Contact Us</div>
+      <div>
+        <img src="../../assets/org.png" alt="img" />
+        <p>
+          We are a popular and farming company aspiring to be a leader in the Organic food industry.
+        </p>
+      </div>
+      <div>
+        <h1>Utility Pages</h1>
+        <p>Style Guide</p>
+        <p>Protected</p>
+        <p>Page Not Found</p>
+        <p>Changelog</p>
+        <p>Licenses</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
