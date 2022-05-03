c5283e1750e35a4ef3105c38680043a417904b13
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-03 11:16:44 +0300

    some changes in OtherProducts

--- src/components/OtherProducts.js
+++ src/components/OtherProducts.js
@@ -1,5 +1,34 @@
 let OtherProducts = () => {
-  return <div>Other Products</div>;
+  return (
+    <div className="bg-[#274C5B]">
+      <div className="flex">
+        <div>
+          <p>Other Products</p>
+          <p>We Offer Organic For You</p>
+        </div>
+        <div>
+          <button>View All Products</button>
+        </div>
+      </div>
+      <div className="flex">
+        <div>
+          <button>Vegetable</button>
+          <img src="../../assets/Tomato.png" alt="img" />
+          <p>Vegan Red Tomato</p>
+        </div>
+        <div>
+          <button>Fruits</button>
+          <img src="../../assets/Almonds.png" alt="img" />
+          <p>Organic Almonds</p>
+        </div>
+        <div>
+          <button>Vegetable</button>
+          <img src="../../assets/Corn.png" alt="img" />
+          <p>Fresh Corn</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OtherProducts;
