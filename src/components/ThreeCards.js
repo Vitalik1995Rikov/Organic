2ca8007d868bf80526f8b3ddd85f710724fe16d2
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-03 11:01:29 +0300

    some changes in ThreeCards component

--- src/components/ThreeCards.js
+++ src/components/ThreeCards.js
@@ -1,5 +1,20 @@
 let ThreeCards = () => {
-  return <div>Tree</div>;
+  return (
+    <div className="flex my-36">
+      <div>
+        <img src="../../assets/First.png" alt="img" className="w-96 pr-8" />
+        <button>Fruit Juice</button>
+      </div>
+      <div>
+        <img src="../../assets/Second.png" alt="img" className="w-96 pr-8" />
+        <button>Organic Food</button>
+      </div>
+      <div>
+        <img src="../../assets/Third.png" alt="img" className="w-96" />
+        <button>Nuts Cookies</button>
+      </div>
+    </div>
+  );
 };
 
 export default ThreeCards;
