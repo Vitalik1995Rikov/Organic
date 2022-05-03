c1f4e7c3aa7953fd29489b851f34483212897297
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-03 11:06:11 +0300

    some changes in EcoFriendly component

--- src/components/EcoFriendly.js
+++ src/components/EcoFriendly.js
@@ -1,5 +1,31 @@
 let EcoFriendly = () => {
-  return <div>Eco</div>;
+  return (
+    <div className="flex">
+      <div className="w-1/2">
+        <img src="../../assets/Nature.png" alt="img" />
+      </div>
+      <div>
+        <p>Eco Friendly</p>
+        <p>From our Farm</p>
+        <p>to your Home.</p>
+        <p>Choose Your Products</p>
+        <p>
+          In our listing, we have several collections of organic products. This is the place where
+          you need to choose the product you want.
+        </p>
+        <p>Farmers will produce it</p>
+        <p>
+          The Product that you ordered will be verified that we have or not if have we will start to
+          move on with the next step.
+        </p>
+        <p>We Can Delivery too</p>
+        <p>
+          If you are not comfortable going to the nearby market place we also will deliver your
+          product to your doorstep.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default EcoFriendly;
