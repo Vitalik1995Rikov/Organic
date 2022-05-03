7ad9797afcfd5276e48b38ea04710e279b10ee02
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-03 10:45:29 +0300

    News component refactored

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,21 @@
 let News = () => {
-  return <div>News</div>;
+  return (
+    <div>
+      <h1>News</h1>
+      <div className="flex">
+        <p>Discover The Recent Content About Organic Products</p>
+        <button>More News</button>
+      </div>
+      <div className="flex">
+        <div className="h-96 w-96">
+          <img src="../../assets/Green.png" alt="img" />
+        </div>
+        <div className="h-96 w-96">
+          <img src="../../assets/Red.png" alt="img" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default News;
