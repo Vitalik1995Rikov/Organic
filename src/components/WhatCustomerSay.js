02c1cf687147b820c41d893a5066aafcf299f96c
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-04 20:38:07 +0300

    some changes in WhatCustomerSay

--- src/components/WhatCustomerSay.js
+++ src/components/WhatCustomerSay.js
@@ -1,5 +1,18 @@
 let WhatCustomerSay = () => {
-  return <div>What customer saying</div>;
+  return (
+    <div>
+      <p>Testimonial</p>
+      <p>What customer saying</p>
+      <img src="../../assets/Face.png" alt="img" />
+      <p>
+        The quality of organic produce is extremely high, the service is second to none and the
+        taste of the food
+      </p>
+      <p>takes me back to my childhood when we were growing our own.</p>
+      <p>Sara Taylor</p>
+      <p>Consumer</p>
+    </div>
+  );
 };
 
 export default WhatCustomerSay;
