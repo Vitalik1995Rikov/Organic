e0a4057b11f579c93c3c5bbec6f3ca7438d8c1d6
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-04 20:53:31 +0300

    added array with data

--- src/components/OurProducts.js
+++ src/components/OurProducts.js
@@ -1,5 +1,23 @@
+let array = [
+  { head: 'Vegetable', img: '../../assets/Broccoli.png', name: 'Calabrese Broccoli' },
+  { head: 'Vegetable', img: '../../assets/Corn.png', name: 'Fresh Corn' },
+  { head: 'Mill', img: '../../assets/Pistachio.png', name: 'Dried Pistachio' },
+  { head: 'Vegetable', img: '../../assets/Tomato.png', name: 'Vegan Red Tomato' },
+  { head: 'Fruit', img: '../../assets/Almonds.png', name: 'Organic Almonds' },
+  { head: 'Mill', img: '../../assets/Hazelnut.png', name: 'Brown Hazelnut' },
+];
+
 let OurProducts = () => {
-  return <div>Our Products</div>;
+  return (
+    <ul>
+      {array.map((item) => (
+        <li>
+          <button>{item.head}</button>
+          <img src={item.img} alt="img" />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default OurProducts;
