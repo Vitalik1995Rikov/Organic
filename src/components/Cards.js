939ac5c03d5be568d2694a7058a93a167916f050
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-10 11:19:20 +0300

    changed sizes

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,8 +1,16 @@
 let Cards = () => {
   return (
-    <div className="flex justify-center h-96">
-      <img src="../../assets/One.png" alt="img" className="mr-8" />
-      <img src="../../assets/Two.png" alt="img" />
+    <div className="flex justify-center w-[1380] my-[140px]">
+      <div className="bg-[url('../../assets/One.png')] h-[378px] w-[665px] mr-[50px]">
+        <p>Natural!!</p>
+        <p>Get Garden</p>
+        <p>Fresh Fruits</p>
+      </div>
+      <div className="bg-[url('../../assets/Two.png')] h-[378px] w-[665px]">
+        <p>Other!!</p>
+        <p>Get 10% off</p>
+        <p>on Vegetables</p>
+      </div>
     </div>
   );
 };
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,9 @@ let Welcome = () => {
   return (
     <div className="bg-[url('../../assets/Welcome.png')] h-[913px] p-7 flex flex-col justify-center">
       <p className="text-[#7EB693] text-2xl">Made With Nature</p>
-      <p className="text-[#274C5B] text-4xl">Welcome to the world of nature and organic.</p>
+      <p className="text-[#274C5B] text-7xl">Welcome to the</p>
+      <p className="text-[#274C5B] text-7xl">world of nature and organic.</p>
+      <p className="text-[#274C5B] text-7xl">world of nature and organic.</p>
       <button className="bg-[#EFD372] text-[#274C5B] rounded w-64 h-20">Explore New</button>
     </div>
   );
