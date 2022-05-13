26814933c3636f3be80d4d752df4fcd67096568f
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-13 18:41:55 +0300

    changed content in EcoFriendly

--- src/components/EcoFriendly.js
+++ src/components/EcoFriendly.js
@@ -5,24 +5,26 @@ let EcoFriendly = () => {
         <img src="../../assets/Nature.png" alt="img" />
       </div>
       <div>
-        <p>Eco Friendly</p>
-        <p>From our Farm</p>
-        <p>to your Home.</p>
-        <p>Choose Your Products</p>
-        <p>
-          In our listing, we have several collections of organic products. This is the place where
-          you need to choose the product you want.
-        </p>
-        <p>Farmers will produce it</p>
-        <p>
-          The Product that you ordered will be verified that we have or not if have we will start to
-          move on with the next step.
-        </p>
-        <p>We Can Delivery too</p>
-        <p>
-          If you are not comfortable going to the nearby market place we also will deliver your
-          product to your doorstep.
-        </p>
+        <div className="px-[60px] pt-[39px] pb-[50px] absolute h-[623px] w-[720px] rounded-[30px] ml-[-60px] mt-[40px] z-10 bg-[#FFFFFF]">
+          <p className="text-[#7EB693] text-3xl">Eco Friendly</p>
+          <p className="text-5xl">From our Farm</p>
+          <p className="text-5xl">to your Home.</p>
+          <p className="text-2xl">Choose Your Products</p>
+          <p className="text-[#525C60]">
+            In our listing, we have several collections of organic products. This is the place where
+            you need to choose the product you want.
+          </p>
+          <p className="text-2xl">Farmers will produce it</p>
+          <p className="text-[#525C60]">
+            The Product that you ordered will be verified that we have or not if have we will start
+            to move on with the next step.
+          </p>
+          <p className="text-2xl">We Can Delivery too</p>
+          <p className="text-[#525C60]">
+            If you are not comfortable going to the nearby market place we also will deliver your
+            product to your doorstep.
+          </p>
+        </div>
       </div>
     </div>
   );
