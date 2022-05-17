1cdcf1f83bf538a286942c515138fe352ac5b85a
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-17 17:44:51 +0300

    refactored content in Subscribe component

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -1,10 +1,14 @@
 let Subscribe = () => {
   return (
-    <div className="h-80 bg-[url('../../assets/Subscribe.png')]">
-      <p>Subscribe Our</p>
-      <p>Newsletter</p>
-      <input type="text" />
-      <button>Subscribe</button>
+    <div className="h-[320px] w-[1380px] flex bg-[url('../../assets/Subscribe.png')] py-[70px] px-[100px] mt-[111px] mx-[30px]">
+      <div>
+        <p>Subscribe Our</p>
+        <p>Newsletter</p>
+      </div>
+      <div className="ml-[350px]">
+        <input type="text" />
+        <button>Subscribe</button>
+      </div>
     </div>
   );
 };
