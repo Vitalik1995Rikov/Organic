978b5d59321d535fb571ee3e7b7e3245a0d88348
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2022-05-18 20:33:06 +0300

    Welcome component ready

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,8 +6,10 @@ let Welcome = () => {
       <p className="text-[#274C5B] text-7xl">world of nature</p>
       <p className="text-[#274C5B] text-7xl">and organic.</p>
       <button className="bg-[#EFD372] flex justify-center items-center text-[#274C5B] rounded-2xl w-[219px] h-[82px] mt-[30px]">
-        Explore New
-        <div className="w-[38px] h-[34px] mr-[30px] rounded-full bg-[#EFF6F1]"></div>
+        <p className="ml-[33px]">Explore New</p>
+        <div className="ml-[24px] w-[38px] h-[34px] mr-[30px] rounded-full bg-[#335B6B] flex justify-center items-center">
+          <img src="../../assets/Vector.png" alt="img" />
+        </div>
       </button>
     </div>
   );
